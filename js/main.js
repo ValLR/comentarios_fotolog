bf210570db0312f516dc6c511b7676aa36bd7c80
ValLR <valentinalopezrecabal@gmail.com>
2017-06-18 22:41:03 -0400

    funcion inicial y guardar listas, avance en función imprimir

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 /*aquí va tu código*/
+function initial{
+	var save= document.getElementById("save");
+	save.addEventListener("click", saveData);//saveData es la función que reune values y contiene al localStorage
+
+	print();
+}
+initial();
+
+function saveData(){
+	var clave= document.getElementById("clave").value;
+	var valor=document.getElementById("valor").value;
+
+	localStorage.setItem(clave,valor);
+	print();
+}
+function print(){
+	var container= document.getElementById("ale");
+	contain.innerHTML=""//recarga pag pero no limpia inputs
+}
